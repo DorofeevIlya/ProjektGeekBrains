ff3c583f9c1a4f0959390ba5fe50d86893fed1cc
Ilya Dorofeev <dara321@yandex.ru>
2023-03-10 19:39:22 +0300

    Создание метода для проверки длины строки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿string [] a= {"Cat","Dog","Bird","Snake"};
-
+void VivodArray (string[]a)
+{
+for (int i=0;i<a.Length;i++)
+    {
+        if (a[i].Length<=3)
+        {
+            System.Console.Write($" {a[i]} ");
+        }
+    }
+}
